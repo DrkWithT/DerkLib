5fc14ec7aaa427c2bc0523f0769f3c3c40b4a4ee
drkwitht <derektan3510@gmail.com>
2025-04-03 16:35:05 -0700

    Added more meta helpers, traits.

--- derklib/includes/meta/maths.hpp
+++ derklib/includes/meta/maths.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <concepts>
+#include "meta/general.hpp"
 
 namespace DerkLib::Meta::Maths {
     template <template <typename, std::size_t, std::size_t> typename M, typename T, std::size_t Rows, std::size_t Cols>
@@ -11,4 +12,44 @@ namespace DerkLib::Meta::Maths {
         {arg.at(0, 0)} -> std::same_as<T&>;
         {arg.chop()};
     };
+
+    template <std::size_t ARows, std::size_t ACols, std::size_t BRows, std::size_t BCols>
+    struct are_multipliable_matrix_dims_t {
+        static constexpr auto flag = ACols == BRows;
+    };
+
+    template <std::size_t ARows, std::size_t ACols, std::size_t BRows, std::size_t BCols>
+    constexpr bool AreMatDimsCompatible = are_multipliable_matrix_dims_t<ARows, ACols, BRows, BCols>::flag;
+
+    /**
+     * @brief Deduces a type of a matrix multiplication result or `void` on incompatible matrices.
+     * 
+     * @tparam `A` for 1st operand of matrix product
+     * @tparam `AItem` stored type in matrix A
+     * @tparam `ARows` 
+     * @tparam `ACols` 
+     * @tparam `B` for 2nd operand of matrix product
+     * @tparam `BItem` stored type in matrix B
+     * @tparam `BRows` 
+     * @tparam `BCols` 
+     */
+    template <template <typename, std::size_t, std::size_t> typename A, typename AItem, std::size_t ARows, std::size_t ACols, template <typename, std::size_t, std::size_t> typename B, typename BItem, std::size_t BRows, std::size_t BCols>
+    struct deduce_matrix_product_t {
+        using type = General::choose_type_t<AreMatDimsCompatible<ARows, ACols, BRows, BCols>, A<AItem, ARows, BCols>, void>;
+    };
+
+    /**
+     * @brief Alias for `deduce_matrix_product_t<...>::type`.
+     * 
+     * @tparam `A`
+     * @tparam `AItem`
+     * @tparam `ARows`
+     * @tparam `ACols`
+     * @tparam `B`
+     * @tparam `BItem`
+     * @tparam `BRows`
+     * @tparam `BCols`
+     */
+    template <template <typename, std::size_t, std::size_t> typename A, typename AItem, std::size_t ARows, std::size_t ACols, template <typename, std::size_t, std::size_t> typename B, typename BItem, std::size_t BRows, std::size_t BCols>
+    using ProductOfMatrices = typename deduce_matrix_product_t<A, AItem, ARows, ACols, B, BItem, BRows, BCols>::type;
 }
